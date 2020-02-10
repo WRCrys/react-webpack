f80aefce7ee361da8efdc466ae4e960c34d514de
Crystyano Almeida <winnerbyjesus@gmail.com>
2020-02-10 01:22:50 -0300

    M1#A12 - Renderizando componentes com classes do ES6/2015

--- src/app.js
+++ src/app.js
@@ -1,30 +1,41 @@
 'use strict'
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './title'
 
 // var React = require('react')
 
-const App = React.createClass({
-    render: function () {
+class App extends Component {
+    render() {
         return (
-            /**
-             * As únicas excessões dos atributos html no react são:
-             * className (React) = class (html)
-             * htmlFor (React) = for (html)
-             */
             <div className='container'>
-                {/* Para conseguirmos passar valores sem ser string, pois o JSX espera uma string
-                coloca-se as chaves simbolizando uma expressão e dentro passamos o valor que precisamos
-                como mostra o atributo 'lastname' */}
-                {/* <Title name='Bruce' lastname={Number(9457)} /> */}
-                {/* <Title name='Bruce' lastname={[1,2,3,4,5,6]} /> */}
                 <Title name='Bruce' lastname={{ first: 'Way', last: 'ne' }} />
-                <label htmlFor='input'>Input </label>
-                <input type='text' id='input' arial-hidden={true} />
             </div>
         )
     }
-})
+}
+
+
+// const App = React.createClass({
+//     render: function () {
+//         return (
+//             /**
+//              * As únicas excessões dos atributos html no react são:
+//              * className (React) = class (html)
+//              * htmlFor (React) = for (html)
+//              */
+//             <div className='container'>
+//                 {/* Para conseguirmos passar valores sem ser string, pois o JSX espera uma string
+//                 coloca-se as chaves simbolizando uma expressão e dentro passamos o valor que precisamos
+//                 como mostra o atributo 'lastname' */}
+//                 {/* <Title name='Bruce' lastname={Number(9457)} /> */}
+//                 {/* <Title name='Bruce' lastname={[1,2,3,4,5,6]} /> */}
+//                 <Title name='Bruce' lastname={{ first: 'Way', last: 'ne' }} />
+//                 <label htmlFor='input'>Input </label>
+//                 <input type='text' id='input' arial-hidden={true} />
+//             </div>
+//         )
+//     }
+// })
 
 // module.exports = Title
 export default App
