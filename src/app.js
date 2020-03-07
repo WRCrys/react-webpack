f99e043a18b338f92d03816583a7f52e861ccce0
Crystyano Almeida <winnerbyjesus@gmail.com>
2020-03-07 00:36:21 -0300

    [M1#A30] - Formulário (select e options)

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            value: 'Valor inicial',
-            checked: false
+            value: '2'
         }
     }
 
@@ -15,26 +14,15 @@ class App extends Component {
         return (
             <div>
                 <form>
-                    <input type='text' value={this.state.value} onChange={(e) => {
+                    <select value={this.state.value} onChange={(e) => {
                         this.setState({
                             value: e.target.value
                         })
-                    }} />
-
-                    <label>
-                        <input
-                            type='checkbox'
-                            value='my-checkbox'
-                            checked={this.state.checked}
-                            onChange={(e) => { 
-                                this.setState({ checked: e.target.checked }) 
-                                }} />
-                        Checkbox
-                </label>
-
-                <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
-                <input type='radio' name='rd' value='2' /> Radio 2
-
+                    }}>
+                        <option value='1'>Opção 1</option>
+                        <option value='2'>Opção 2</option>
+                        <option value='3'>Opção 3</option>
+                    </select>
                 </form>
             </div>
         )
